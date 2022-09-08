d7c564db0c0d3319b1b098603f07bfc03af0b2bb
SeldomseenSchweig <jamesschweig@gmail.com>
2022-09-08 09:50:06 -0700

    more jest tests

--- square.test.js
+++ square.test.js
@@ -5,3 +5,9 @@ test('square should return the square value of a number', function(){
     const res = square(3)
     expect(res).toEqual(9)
 } )
+
+
+test('square should return square negative numbers', function(){
+    const num = square(-9)
+    expect(num).toEqual(81)
+} )
